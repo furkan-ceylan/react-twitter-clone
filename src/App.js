76a51e0a710b578f77ddcd8ec8680729bc890e62
furkan ceylan <ceylan.furkan@outlook.com>
2021-09-02 02:26:24 +0300

    Add profile route and RightSidebar component to app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './styles/App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
+import Profile from './pages/Profile'
 import Sidebar from './components/Sidebar'
+import RightSidebar from './components/RightSidebar'
 
 function App() {
   return (
@@ -14,8 +16,12 @@ function App() {
         <div>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/profile" component={Profile} />
             {/* <Route path="/tweet/:id" component={Tweet} /> */}
           </Switch>
+        </div>
+        <div className="right-side">
+          <RightSidebar />
         </div>
       </div>
     </Router>
