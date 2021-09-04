699de4abf9e9a410b3b0cd66d334f1ef6073c324
furkan ceylan <ceylan.furkan@outlook.com>
2021-09-05 02:57:47 +0300

    Add default object of new tweet

--- src/state/reducers/tweetReducer.js
+++ src/state/reducers/tweetReducer.js
@@ -1,7 +1,21 @@
 export const tweetReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TWEET':
-      return [...state, action.payload]
+      const id = 3
+      const addTweet = {
+        id: id,
+        user: {
+          name: 'John Doe',
+          image:
+            'https://pbs.twimg.com/profile_images/1384273229498384388/5hQ2wv3H_400x400.jpg',
+          handle: '@johndoe',
+        },
+        tweet: action.payload,
+      }
+      return {
+        ...state,
+        store: [addTweet, ...state.store],
+      }
     case 'REMOVE_TWEET':
       return state.filter((tweet) => tweet.id !== action.payload)
     default:
