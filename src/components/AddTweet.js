706a2dcad13872d8f2cc69e17c9bccae9b7fa8fc
furkan ceylan <ceylan.furkan@outlook.com>
2021-09-05 02:57:12 +0300

    Add tweet to store state

--- src/components/AddTweet.js
+++ src/components/AddTweet.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { SmallAvatar } from '../images/avatars'
 import {
   AddImageIcon,
@@ -6,10 +6,42 @@ import {
   AddPollIcon,
   AddEmojiIcon,
 } from '../images/svg/svgs'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../state/index.js'
+import { bindActionCreators } from 'redux'
 
 export const AddTweet = () => {
   const profileImageUrl =
     'https://pbs.twimg.com/profile_images/1384273229498384388/5hQ2wv3H_400x400.jpg'
+
+  let [loading, setLoading] = useState(false)
+
+  const handleChange = (e) => {
+    const { text, value } = e.target
+    setTweetData((prevState) => ({
+      ...prevState,
+      text: value,
+    }))
+  }
+  const [tweetData, setTweetData] = useState({
+    image: '',
+    time: '1m',
+    retweets: 0,
+    likes: 0,
+    comments: [],
+  })
+
+  const dispatch = useDispatch()
+  const { addTweet } = bindActionCreators(actionCreators, dispatch)
+  const handleSubmit = () => {
+    setLoading(true)
+    addTweet(tweetData)
+    setLoading(false)
+    setTweetData((prevState) => ({
+      ...prevState,
+      text: '',
+    }))
+  }
 
   return (
     <div className="new-tweet">
@@ -23,6 +55,8 @@ export const AddTweet = () => {
               className="w-100"
               placeholder="What's happening?"
               type="text"
+              onChange={handleChange}
+              value={tweetData.text}
             />
           </span>
         </div>
@@ -33,8 +67,26 @@ export const AddTweet = () => {
             <AddPollIcon />
             <AddEmojiIcon />
           </div>
-          <div className="tweet">
-            <div className="btn tweet-btn text-center">Tweet</div>
+          <div className="tweet" onClick={handleSubmit}>
+            {loading ? (
+              <button
+                disabled
+                className="btn tweet-btn text-center btn-disabled"
+              >
+                Loading
+              </button>
+            ) : (
+              <button
+                disabled={!tweetData.text}
+                className={
+                  tweetData.text
+                    ? 'btn tweet-btn text-center'
+                    : 'btn tweet-btn text-center btn-disabled'
+                }
+              >
+                Tweet
+              </button>
+            )}
           </div>
         </div>
       </div>
